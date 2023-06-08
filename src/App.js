e1d4cf5e9a5629d264b5776e7668ebb8c09e089a
m25703 <114353660+m25703@users.noreply.github.com>
2023-06-08 19:47:03 +0530

    fc

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ const App = () => {
   <h1> .. </h1>
   //let story = "He stared out the window at the snowy field. He'd been stuck in the house for close to a month and his only view of the outside world was through the window. There wasn't much to see. It was mostly just the field with an occasional bird or small animal who ventured into the field. As he continued to stare out the window, he wondered how much longer he'd be shackled to the steel bar inside the house";
   //let story = "All the world is a stage and all men and women merely players, They have their exits and their entrances and one man in his time plays many parts.";
-  let story = "We stay in a big house. It has a red wall. There are brown doors."
+  let story = "We stay in a big house. It has red walls. There are brown doors."
   const words = story.split(" ");
 
   return (
